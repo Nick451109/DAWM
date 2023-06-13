7ab9c8285c1a14443efb4293d65cecf5375a04e5
Nick451109 <narevalo@espol.edu.ec>
2023-06-12 19:56:13 -0500

    soluciones
    Cap 3 Ejercicios 6-9

--- Capítulo 03/ejercicio06/public/scripts/ejercicio.js
+++ Capítulo 03/ejercicio06/public/scripts/ejercicio.js
@@ -49,6 +49,34 @@ let animarCartas = (carta) => {
 
 /*
     Revisar 
+
+      callback es una funcion dentro de otra funcion 
+      si una funcion retorna una promesa se puede poner await en frente de la llamada a la funcion
+      await solo se puede usar dentro de funciones asincronas
+      await esperara a que la promesa retornada de la funcion asincrona, termine 
+
+      async se usa para definir funciones asincronas y estas siempre retornan promesas
+      
+      for in se usa para iterar en las propiedades de un objeto
+
+      *Recorrer HTMLColletions
+      for of se usa para iterar arrays, strings, nodeLists, and HTMLCollections -> (getElementById(), ...class(),....tag(), etc)
+      for regular
+      convetir la coleccion en un arreglo con array.from() y luego recorrerlo con for each 
+                          Array.from(collection).forEach(function (element) {
+                        console.log(element)
+                    });
+
+      *Hacer fetch con xml
+                                fetch("https://codetogo.io/api/users.xml")
+                            .then(response => response.text())
+                            .then(data => {
+                              const parser = new DOMParser();
+                              const xml = parser.parseFromString(data, "application/xml");
+                              console.log(xml);
+                            })
+                            .catch(console.error);
+
         https://www.javascripttutorial.net/javascript-fetch-api/
         https://developer.mozilla.org/es/docs/Web/API/Document/querySelectorAll
         https://www.geeksforgeeks.org/htmlcollection-for-loop/
@@ -61,7 +89,7 @@ let animarCartas = (carta) => {
 // Petición async/await
 // Convierta la función flecha como async
 
-let cargarLibros = () => {
+let cargarLibros = async() => {
 
     let plantilla, arreglo, contenedor;
 
@@ -69,25 +97,27 @@ let cargarLibros = () => {
    
     // Petición async/await
     // Anteponga await a la petición fetch   
-    let resultado = fetch("https://dataserverdawm.herokuapp.com/libros/xml")
+    let resultado = await fetch("https://github.com/DAWMFIEC/DAWM-apps/blob/datos/libros.xml")
+    
+    //let resultado = await fetch("https://dataserverdawm.herokuapp.com/libros/xml")
 
     // Anteponga await a la conversión de la variable resultado a texto
-    let data = resultado.text()
+    let data = await resultado.text()
 
     const parser = new DOMParser();
     const xml = parser.parseFromString(data, "application/xml");
   
     // Utilice el selector 'libros > libro'
-    let books = xml.querySelectorAll('')
+    let books = xml.querySelectorAll('libros > libro')
 
     books.forEach( book => {
 
       // Extraiga el contenido del texto para las etiquetas: title, isbn y shortDescription.
       // Utilice el operador condicional ternario para validar la existencia de las etiquetas previo a extraer el contenido del texto
       let thumbnailUrl = book.querySelector('thumbnailUrl')?book.querySelector('thumbnailUrl').textContent:''
-      let title = ''
-      let isbn = ''
-      let shortDescription = ''
+      let title = book.querySelector('title')?book.querySelector('title').textContent:''
+      let isbn = book.querySelector('isbn')?book.querySelector('isbn').textContent:''
+      let shortDescription = book.querySelector('shortDescription')?book.querySelector('shortDescription').textContent:''
       
 
       plantilla = `
